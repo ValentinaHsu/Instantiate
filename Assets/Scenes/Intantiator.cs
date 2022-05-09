19fbd81a8bae2ee2ce2f36cf57c767c2b6d5d706
ValentinaHsu <92767924+ValentinaHsu@users.noreply.github.com>
2022-05-09 11:49:34 -0300

    initiator

---
+++ Assets/Scenes/Intantiator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    public void CloneObject()
+    {
+        Instantiate(objectToClone);
+    }
+}
